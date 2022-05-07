826c196668ae69909409fc5b0b16a14a3a7f0b27
maiscafe <adriano_tomaban@hotmail.com>
2022-05-07 14:44:52 -0300

    finish

--- js/importar-pacientes.js
+++ js/importar-pacientes.js
@@ -6,13 +6,23 @@ botaoAdicionar.addEventListener("click", function() {
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
     xhr.send()
     xhr.addEventListener("load", function (){
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
-        console.log(pacientes);
+        var erroAjax = xhr.status
+        if (erroAjax == 200) {
+            
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(function(paciente) {
+                adicionaPacientes(paciente)
+            })
+        } else {
+            var exibeErros = document.querySelector("#exibe-erro");
+            exibeErros.classList.remove("invisivel");
+            exibeErros.textContent = "Erro ao importar os pacientes";
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
         
-        pacientes.forEach(function(paciente) {
-            adicionaPacientes(paciente)
-        })
+
 
     });
 
